ebf090836b90dba9964b91099157dc140aa09734
imad201-coder <imadammour44@gmail.com>
2026-09-23 21:43:20 +0100

    Change default language from English to Arabic

--- v2/smartbags/js/i18n.js
+++ v2/smartbags/js/i18n.js
@@ -9,7 +9,7 @@
    exactly as entered, in whichever language you typed it. */
 
 const LANG_KEY = 'smartbags_lang';
-const DEFAULT_LANG = 'en';
+const DEFAULT_LANG = 'ar';
 
 const TRANSLATIONS = {
   en: {
